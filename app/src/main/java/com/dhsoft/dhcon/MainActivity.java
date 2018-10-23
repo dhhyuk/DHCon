a9b74343a20cbf4a44fb666ff93fed86ee578f15
DongHyuk Kim <sss989870@gmail.com>
2018-10-24 06:00:29 +0900

    Bitmap에 Text 그리기

--- app/src/main/java/com/dhsoft/dhcon/MainActivity.java
+++ app/src/main/java/com/dhsoft/dhcon/MainActivity.java
@@ -1,8 +1,14 @@
 package com.dhsoft.dhcon;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +18,10 @@ import android.widget.ImageView;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ImageView imageView;
+
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +62,86 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 if (bitmap == null) return;
+                this.mBitmap = bitmap;
 
-                imageView.setImageBitmap(bitmap);
+                final Bitmap textDrawedBitmap = drawTextOnBitmap(mBitmap, "TEST");
+                imageView.setImageBitmap(textDrawedBitmap);
+
+                saveBitmapInDirectory(getApplicationContext(), textDrawedBitmap);
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
                 Log.e(TAG, "CropImage onActivityResult Exception : " + error.getMessage());
             }
 
         }
+    }
+
+    public Bitmap drawTextOnBitmap(Bitmap bitmap, String text) {
+
+        Bitmap.Config bitmapConfig = bitmap.getConfig();
+        if (bitmapConfig == null) bitmapConfig = Bitmap.Config.ARGB_8888;
+
+        bitmap = bitmap.copy(bitmapConfig, true);
+
+
+        float fontSize = bitmap.getWidth() / 4;
+        if (text.length() > 4) {
+            fontSize = bitmap.getWidth() / text.length();
+        }
+
+
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(fontSize);
+
+        Paint strokePaint = new Paint();
+        strokePaint.setStyle(Paint.Style.STROKE);
+        strokePaint.setStrokeWidth(0.5f);
+        strokePaint.setColor(Color.GRAY);
+        strokePaint.setTextSize(fontSize);
+
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+
+        canvas.drawText(text, canvas.getWidth() / 2 - bounds.width() / 2, bitmap.getHeight() - bounds.height() / 2 + 2, paint);
+        canvas.drawText(text, canvas.getWidth() / 2 - bounds.width() / 2, bitmap.getHeight() - bounds.height() / 2 + 2, strokePaint);
+
+        return Bitmap.createScaledBitmap(bitmap, 128, 128, true);
+    }
+
+    public static void saveBitmapInDirectory(Context context, Bitmap bitmap) {
+        final String externalPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        final String folderName = "/DHCon/";
+        final String fileName = new Date().toString() + ".jpg";
+
+        final String path = externalPath + folderName;
+
+        File filePath;
+        try {
+            filePath = new File(path);
+            if (!filePath.isDirectory()) {
+                Log.d("ImageEditor", "isDirectory : " + filePath.mkdirs());
+            }
+            FileOutputStream out = new FileOutputStream(path + fileName);
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.close();
+
+            galleryAddPic(context, path + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void galleryAddPic(Context context, String currentPhotoPath) {
+        Log.d("ImageEditor", "galleryAddPic");
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Log.d("ImageEditor", "gallery exist : " + f.exists());
+
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        context.sendBroadcast(mediaScanIntent);
     }
 }
